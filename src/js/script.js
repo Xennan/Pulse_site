99715fe55ac57a0e737babd434475b42a58d035a
Xennan <darkquantumgames@gmail.com>
2024-09-01 23:54:48 +0300

    Pulse step 3 (slider 1st var + adaptive)

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,17 @@
-if (2*4 == 8*1) {
-    console.log("Верно");
-}
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 700,
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"</button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"</button>',
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    dots: true,
+                    arrows: false
+            }
+        }
+        ]
+    });
+});
